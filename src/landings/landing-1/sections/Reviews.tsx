a834052ccaaf273c07f43ae9963e3edc252e93ce
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-11-18 01:52:45 +0200

    chore (landings and base): minor changes

---
+++ src/landings/landing-1/sections/Reviews.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { reviewBusnes, text, reviewsCards } from "../config.ts";
+
+const Reviews: React.FC = () => {
+	return <section></section>;
+};
+
+export { Reviews };
